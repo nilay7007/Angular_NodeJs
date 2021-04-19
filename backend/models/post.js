80923865a528f5d43093daa695ee05a34d29c6ac
nilay personal <nilaysachan16@gmail.com>
2021-04-19 18:50:05 +0530

    Working with mongoose to add delete our items

---
+++ backend/models/post.js
@@ -0,0 +1,16 @@
+
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+    title:{ type:String, required: true},
+    content:{ type:String, required: true}
+});
+// after schema mogoose needs models to work with it.
+//schema is blueprint.Not a thing we work with our code.PointAt Just a blueprint
+//now to create model(or object)
+
+module.exports=mongoose.model('Post',postSchema);
+//collection name will be plural form of post in loewercase ->posts
+
+//after these model has been created it can be used to create javascript objects based on that model
+//this .model('Post',postSchema); gives us the constructor to allow to construct new java object

2f99010696966acd15e8fbefdd30582a8d89bd79
nilay personal <nilaysachan16@gmail.com>
2021-04-20 13:08:36 +0530

    Editing items , Spinners and refactoring routes

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -4,13 +4,15 @@ import { Subject } from "rxjs";
 import { map } from 'rxjs/operators';
 
 import { Post } from "./post.model";
+import { Router } from "@angular/router";
 
 @Injectable({ providedIn: "root" })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+    private router: Router) {}
 
   getPosts() {
     this.http
@@ -54,6 +56,28 @@ export class PostsService {
         const updatedPosts = this.posts.filter(post => post.id !== postId);
         this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  getPost(id: string) {
+    return this.http.get<{ _id: string; title: string; content: string }>(
+      "http://localhost:3000/api/posts/" + id
+    );
+    //return {...this.posts.find(p=>p.id==id)};
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http
+      .put("http://localhost:3000/api/posts/" + id, post)
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+        this.router.navigate(["/"]);
+        console.log(response);
       });
   }
 }

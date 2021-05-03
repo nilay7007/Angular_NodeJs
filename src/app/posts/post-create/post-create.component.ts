ee94a4f223e399eb233e9423e6cfed59c689fc79
nilay personal <nilaysachan16@gmail.com>
2021-05-03 17:17:09 +0530

    Setting isLoading = false on creating a new post

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { FormControl, FormGroup, NgForm, Validators } from "@angular/forms";
 import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Subscription } from "rxjs";
+import { AuthService } from "src/app/auth/auth.service";
 import { Post } from "../post.model";
 import { PostsService } from "../posts.service";
 import { mimeType } from "./mime-type.validator";
@@ -10,7 +12,7 @@ import { mimeType } from "./mime-type.validator";
   templateUrl: "./post-create.component.html",
   styleUrls: ["./post-create.component.css"]
 })
-export class PostCreateComponent implements OnInit {
+export class PostCreateComponent implements OnInit, OnDestroy  {
   enteredTitle = "";
   enteredContent = "";
   post: Post;
@@ -19,10 +21,18 @@ export class PostCreateComponent implements OnInit {
   private postId: string;
   form: FormGroup;
   imagePreview: string;
+  private authStatusSub: Subscription;
+
   constructor(public postsService: PostsService,
-    public route: ActivatedRoute) { }
+    public route: ActivatedRoute,
+    private authService: AuthService) { }
 
   ngOnInit() {
+    this.authStatusSub = this.authService
+    .getAuthStatusListener()
+    .subscribe(authStatus => {
+      this.isLoading = false;
+    });
     this.form = new FormGroup({
       title: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)]
@@ -124,5 +134,9 @@ title: null
       this.imagePreview = reader.result as string;
     };
     reader.readAsDataURL(file);
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 }

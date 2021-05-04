c14f18e28598ae0332712888d79574e9ea4a993d
nilay personal <nilaysachan16@gmail.com>
2021-05-04 18:02:10 +0530

    Adding controllers for better code optimization

---
+++ backend/middleware/file.js
@@ -0,0 +1,36 @@
+const multer = require("multer");
+
+const MIME_TYPE_MAP = {
+    "image/png": "png",
+    "image/jpeg": "jpg",
+    "image/jpg": "jpg"
+};
+//where multer shud put files when it encounetrs in incoming request
+// destination:is a functions when multer tries to save the file it detects
+//file, cb:file is what it detectd.cb is where it will save the file
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const isValid = MIME_TYPE_MAP[file.mimetype];
+        //above is to check we r getting a suitable file i.e. png jpeg just an extr a security check
+        let error = new Error("Invalid mime type");
+        if (isValid) {
+            error = null;
+        }
+        cb(error, "backend/images");
+        //cb(error, "backend/images");->error or null ->whether u encountered an error
+        //"backend/images"->where u want to save
+        //interstingily backend/images is seen relative to server.js file
+        //file.mimetype will be in images/jpg images/png etc.
+    },
+    filename: (req, file, cb) => {
+        const name = file.originalname
+            .toLowerCase()
+            .split(" ")
+            .join("-");
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        cb(null, name + "-" + Date.now() + "." + ext);
+        //create  a file name -> name + "-" + Date.now() + "." + ext
+    }
+});
+
+module.exports = multer({ storage: storage }).single("image");
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,168 +1,33 @@
 const express = require("express");
-const multer = require("multer");
-const Post = require("../models/post");
+const extractFile = require("../middleware/file");
 const checkAuth = require("../middleware/check-auth");
+const PostController = require("../controllers/posts");
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg"
-};
-//where multer shud put files when it encounetrs in incoming request
-// destination:is a functions when multer tries to save the file it detects
-//file, cb:file is what it detectd.cb is where it will save the file
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    //above is to check we r getting a suitable file i.e. png jpeg just an extr a security check
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images");
-    //cb(error, "backend/images");->error or null ->whether u encountered an error
-    //"backend/images"->where u want to save
-    //interstingily backend/images is seen relative to server.js file
-    //file.mimetype will be in images/jpg images/png etc.
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname
-      .toLowerCase()
-      .split(" ")
-      .join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext);
-    //create  a file name -> name + "-" + Date.now() + "." + ext
-  }
-});
 
 //("",multer({ storage: storage }).single("image"), (req, res, next)->executes functions from left to right
 //single->expecting a single file and will try to find in "image" property of request bodyi.e. postData.append("image"
 router.post("",
   checkAuth,
-  multer({ storage: storage }).single("image"),
-  (req, res, next) => {
-    const url = req.protocol + "://" + req.get("host");
-    const post = new Post({
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: url + "/images/" + req.file.filename,
-      creator: req.userData.userId
-    });
-    post.save().then(createdPost => {
-      res.status(201).json({
-        message: "Post added successfully",
-        post: {
-          ...createdPost,
-          id: createdPost._id
-        }
-      });
-      })
-      .catch(error => {
-        res.status(500).json({
-          message: "Creating a post failed!"
-        });
-      });
-  }
+  extractFile,
+  PostController.createPost
 );
 
 router.put("/:id",
   checkAuth,
-  multer({ storage: storage }).single("image"),
-  (req, res, next) => {
-    let imagePath = req.body.imagePath;
-    if (req.file) {
-      const url = req.protocol + "://" + req.get("host");
-      imagePath = url + "/images/" + req.file.filename;
-    }
-    const post = new Post({
-      _id: req.body.id,
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: imagePath,
-      creator: req.userData.userId
-    });
-    Post.updateOne({ _id: req.params.id , creator: req.userData.userId }, post).then(result => {
-      if (result.nModified > 0) {
-        res.status(200).json({ message: "Update successful!" });
-      } else {
-        res.status(401).json({ message: "Not authorized!" });
-      }
-      })
-      .catch(error => {
-        res.status(500).json({
-          message: "Couldn't udpate post!"
-        });
-      });
-  }
+  extractFile,
+  PostController.updatePost
 );
 
-router.get("", (req, res, next) => {
-  //for pagination w e can fetch those detailsa as query parameter
-  //if we re extrcating querie s or another from url then it will be a string
-  //so connvert it to int by +
-  const pageSize = +req.query.pagesize;
-  const currentPage = +req.query.page;
-  const postQuery = Post.find();//it will be executed when .then is there(one functionality provided by mongoose to split 
-  //queries)
-  let fetchedPosts;
-  if (pageSize && currentPage) {
-    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
-    //if on cuurentpage 3 with pagesize 10 then skippped 20 items
-    //limit to fetch these number of documnets from collections
-  }
-  postQuery
-    .then(documents => {
-      fetchedPosts = documents;
-      return Post.count();
-      //returned an promise
-    })
-    .then(count => {
-      res.status(200).json({
-        message: "Posts fetched successfully!",
-        posts: fetchedPosts,
-        maxPosts: count
-      });
-    })
-    .catch(error => {
-      res.status(500).json({
-        message: "Fetching posts failed!"
-      });
-    });
-});
+router.get("", PostController.getPosts);
 
-router.get("/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: "Post not found!" });
-    }
-  }).catch(error => {
-    res.status(500).json({
-      message: "Fetching post failed!"
-    });
-  });
-});
+router.get("/:id", PostController.getPost);
 
 //passing a refeence of checkAuth ,not the function
 router.delete("/:id",
   checkAuth,
-  (req, res, next) => {
-    Post.deleteOne({ _id: req.params.id }).then(result => {
-      console.log(result);
-      if (result.n > 0) {
-        res.status(200).json({ message: "Deletion successful!" });
-      } else {
-        res.status(401).json({ message: "Not authorized!" });
-      }
-    }).catch(error => {
-      res.status(500).json({
-        message: "Deleting posts failed!"
-      });
-    });
-  });
+  PostController.deletePost
+);
 
 module.exports = router;
